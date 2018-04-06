a2221f3897e628a6dfc3c414988d6399adc1dc2d
Earnest Cain <earnestcain@Earnests-MBP.attlocal.net>
2018-04-06 09:13:36 -0700

    added ui elements and completed initial app logic

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Earnest Cain on 4/5/18.
+//  Copyright © 2018 Earnest Cain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceNumbersArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+       updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceNumbersArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceNumbersArray[randomDiceIndex2])
+    }
+}
+
